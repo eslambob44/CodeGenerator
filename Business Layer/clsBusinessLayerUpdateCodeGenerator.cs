8f9977323ec40c7f281e9cc8884175be02041c7d
eslam yasser <eyasser237@gmail.com>
2025-07-20 18:34:57 +0300

    Implement Business Layer Update Code Generator

---
+++ Business Layer/clsBusinessLayerUpdateCodeGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    public class clsBusinessLayerUpdateCodeGenerator : IBusinessLayerCodeGenerator
+    {
+        private string GenerateFunction(ITableInfo Table , string ObjectName)
+        {
+            string Code = $@"private bool _Update()
+{{
+    return cls{ObjectName}Data.Update({string.Join("," , Table.Columns.Keys)});
+}}";
+            return Code;
+        }
+
+
+
+        public string GenerateCode(ITableInfo Table, string ObjectName)
+        {
+
+            return GenerateFunction(Table , ObjectName);
+
+        }
+    }
+}
